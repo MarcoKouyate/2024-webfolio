8c31c213a10dfbfd68587b2c0207f6128e767ba4
Marco Kouyaté <marco.kouyate@yahoo.fr>
2024-01-02 00:36:11 +0100

    Added Background Video

--- app/components/BackgroundVideo.tsx
+++ app/components/BackgroundVideo.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const BackgroundVideo = () => {
-  return <div>BackgroundVideo</div>;
+  return (
+    <>
+      <video
+        autoPlay
+        muted
+        playsInline
+        loop
+        id="backgroundVideo"
+        className="fixed bottom-0 right-0 min-w-full min-h-full -z-20 object-cover"
+      >
+        <source
+          src="videos/MarcoKouyate_Website_Background.mp4"
+          type="video/mp4"
+        />
+      </video>
+    </>
+  );
 };
 
 export default BackgroundVideo;
