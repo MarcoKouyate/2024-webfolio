4c61c922b1a4a79934383f476db02f89b6d0b310
Marco Kouyaté <marco.kouyate@yahoo.fr>
2024-01-03 23:30:19 +0100

    Menu ( Horizontal Buttons )

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+interface Props {
+  children: ReactNode;
+  link: string;
+}
+
+const Button = ({ children, link }: Props) => {
+  return (
+    <a href={link} target="_blank">
+      <div className="bg-gradient-to-l from-sky-500 to-indigo-500 hover:from-sky-500 hover:to-cyan-500 bg-cover w-fit px-3 py-2 rounded-full text-white font-bold drop-shadow-md opacity-50 hover:opacity-100">
+        {children}
+      </div>
+    </a>
+  );
 };
 
 export default Button;
--- app/components/HorizontalButtons.tsx
+++ app/components/HorizontalButtons.tsx
@@ -3,11 +3,13 @@ import Button from "./Button";
 
 const HorizontalButtons = () => {
   return (
-    <>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-    </>
+    <div className="flex justify-around max-w-96 m-auto mt-8">
+      <Button link="https://www.linkedin.com/in/marcokouyate/">LinkedIn</Button>
+      <Button link="https://github.com/MarcoKouyate">Github</Button>
+      <Button link="https://marcokouyate.notion.site/e15976482eea44d9a419cde7c7b0d0c6">
+        Portfolio
+      </Button>
+    </div>
   );
 };
 
