c994743cdfcc9391635ee32d829d741be667e814
Marco Kouyaté <marco.kouyate@yahoo.fr>
2024-01-07 02:59:38 +0100

    Import react-three fiber and drei + simple 3D cube rendering

--- app/3d/page.tsx
+++ app/3d/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+"use client";
+
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import Loader from "./components/Loader";
 
 const App_3D = () => {
-  return <div className="bg-slate-300">App_3D</div>;
+  return (
+    <section className="h-screen w-full relative bg-slate-300">
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <mesh rotation={[5, 12, 70]}>
+            <boxGeometry args={[2, 2, 2]} />
+            <meshStandardMaterial />
+          </mesh>
+          <ambientLight intensity={0.1} />
+          <directionalLight color={"red"} position={[0, 0, 5]} />
+        </Suspense>
+      </Canvas>
+    </section>
+  );
 };
 
 export default App_3D;
