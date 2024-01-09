e89df2be45247aa37bd762904643df4d5fb92123
Marco Kouyaté <marco.kouyate@yahoo.fr>
2024-01-09 23:22:40 +0100

    Imported custom Paper Plane Model into scene

--- app/3d/components/Scene_3D.tsx
+++ app/3d/components/Scene_3D.tsx
@@ -2,11 +2,22 @@ import React from "react";
 import MemoHead from "./MemoHead";
 import Environment from "./Environment";
 import { OrbitControls } from "@react-three/drei";
+import ImportedMesh from "./ImportedMesh";
+import { Plane } from "./Plane";
 
 const Scene_3D = () => {
   return (
     <>
       <MemoHead />
+      {/* <ImportedMesh /> */}
+      <group rotation={[0, 2, 0]}>
+        <Plane
+          position={[0, 0, 5]}
+          rotation={[0, 2, 0]}
+          scale={[0.5, 0.5, 0.5]}
+        />
+      </group>
+
       <Environment />
       <OrbitControls />
     </>
