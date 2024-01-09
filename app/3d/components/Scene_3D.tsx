d18d4c296fa936cf1ce294a05b82a24ee54e40c8
Marco Kouyaté <marco.kouyate@yahoo.fr>
2024-01-10 00:59:24 +0100

    Orbiting Plane around the head

--- app/3d/components/Scene_3D.tsx
+++ app/3d/components/Scene_3D.tsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { OrbitControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+
 import MemoHead from "./MemoHead";
 import Environment from "./Environment";
-import { OrbitControls } from "@react-three/drei";
-import ImportedMesh from "./ImportedMesh";
 import { Plane } from "./Plane";
 
 const Scene_3D = () => {
+  const [planeRotation, setPlaneRotation] = useState(0);
+
+  useFrame(() => {
+    setPlaneRotation(planeRotation + 0.01);
+  });
+
   return (
     <>
       <MemoHead />
       {/* <ImportedMesh /> */}
-      <group rotation={[0, 2, 0]}>
+      <group rotation={[0, planeRotation, 0]}>
         <Plane
           position={[0, 0, 5]}
-          rotation={[0, 2, 0]}
+          rotation={[0, 2, -0.5]}
           scale={[0.5, 0.5, 0.5]}
         />
       </group>
