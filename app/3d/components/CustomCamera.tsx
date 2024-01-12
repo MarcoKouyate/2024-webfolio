9abe155bd9417f70b5e3e06c9d0f6c7b8a367bf6
Marco Kouyaté <marco.kouyate@yahoo.fr>
2024-01-13 00:19:39 +0100

    Manually Set Azimuthal and Polar Angle of OrbitControls

--- app/3d/components/CustomCamera.tsx
+++ app/3d/components/CustomCamera.tsx
@@ -1,23 +1,36 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { OrbitControls } from "@react-three/drei";
 import type { OrbitControls as OrbitControlsImpl } from "three-stdlib";
 import { RefObject } from "react";
+import { useFrame } from "@react-three/fiber";
 
-const custom_controls = (target: RefObject<OrbitControlsImpl>) => {
-  console.log(target.current);
+const custom_controls = (
+  controls: RefObject<OrbitControlsImpl>,
+  isDragging: boolean,
+  angles: number[]
+) => {
+  console.log(controls.current);
+  if (controls.current && !isDragging) {
+    const [x, y] = angles;
+    controls.current.setAzimuthalAngle(x);
+    controls.current.setPolarAngle(y);
+  }
 };
 
 const CustomCamera = () => {
-  const ref = useRef<OrbitControlsImpl>(null);
+  const controls = useRef<OrbitControlsImpl>(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [angles, setAngles] = useState([0, Math.PI / 2]);
+  useFrame((state, delta) => custom_controls(controls, isDragging, angles));
 
   return (
     <OrbitControls
       enablePan={false}
       enableZoom={false}
-      onChange={() => custom_controls(ref)}
-      autoRotate={true}
+      onStart={(e) => setIsDragging(true)}
+      onEnd={(e) => setIsDragging(false)}
       autoRotateSpeed={0.5}
-      ref={ref}
+      ref={controls}
     />
   );
 };
